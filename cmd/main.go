f4fc7c3054785593bb3044f7d28d16ae8e92337f
Mikhail <rybalkov@transtelesoft.com>
2023-10-06 14:19:43 +0300

    Исправлен вывод информации в консоль

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	desc "github.com/MikhailRibalkov/auth/grpc/pkg/auth_v1/pkg/auth_v1"
+	desc "github.com/MikhailRibalkov/auth/pkg/auth_v1/pkg/auth_v1"
 	"github.com/brianvoe/gofakeit"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -19,7 +19,7 @@ type server struct {
 }
 
 func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	log.Printf("Client id: #{req.GetId}")
+	log.Printf("Client id: %d}", req.GetId)
 
 	return &desc.GetResponse{
 		Info: &desc.UserInfo{
@@ -34,18 +34,18 @@ func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetRespon
 }
 
 func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	log.Printf("Client id: #{req.GetId}")
-	return &desc.CreateResponse{}
+	log.Printf("Created user: %v", req.GetUser())
+	return &desc.CreateResponse{}, nil
 }
 
 func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
-	log.Printf("Client id: #{req.GetId}")
+	log.Printf("Updated user, id: %d", req.GetId())
 	return &emptypb.Empty{}, nil
 
 }
 
 func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	log.Printf("Client id: #{req.GetId}")
+	log.Printf("Deleted user, id: %d", req.GetId())
 	return &emptypb.Empty{}, nil
 }
 
