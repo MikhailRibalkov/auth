9a60eb60ac537babe8ca553e0e49eab9df82fe90
MikhailRibalkov <mikhailribalkov@gmail.com>
2023-10-06 16:29:07 +0300

    Правки

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	desc "github.com/MikhailRibalkov/auth/pkg/auth_v1/pkg/auth_v1"
+	deps "github.com/MikhailRibalkov/auth/pkg/auth_v1/pkg/auth_v1"
 	"github.com/brianvoe/gofakeit"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -15,38 +15,38 @@ import (
 const grpcPort = ":8081"
 
 type server struct {
-	desc.UnimplementedAuthV1Server
+	deps.UnimplementedAuthV1Server
 }
 
-func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+func (s *server) Get(ctx context.Context, req *deps.GetRequest) (*deps.GetResponse, error) {
 	log.Printf("Client id: %d", req.GetId())
 
-	return &desc.GetResponse{
-		Info: &desc.UserInfo{
+	return &deps.GetResponse{
+		Info: &deps.UserInfo{
 			Id:        req.GetId(),
 			Name:      gofakeit.Name(),
 			Email:     gofakeit.Email(),
 			CreatedAt: timestamppb.New(gofakeit.Date()),
-			Role:      desc.UserRole_USER,
+			Role:      deps.UserRole_USER,
 			UpdatedAt: timestamppb.New(gofakeit.Date()),
 		},
 	}, nil
 }
 
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+func (s *server) Create(ctx context.Context, req *deps.CreateRequest) (*deps.CreateResponse, error) {
 	log.Printf("Created user: %v", req.GetUser())
 
-	return &desc.CreateResponse{}, nil
+	return &deps.CreateResponse{}, nil
 }
 
-func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+func (s *server) Update(ctx context.Context, req *deps.UpdateRequest) (*emptypb.Empty, error) {
 	log.Printf("Updated user, id: %d", req.GetId())
 
 	return &emptypb.Empty{}, nil
 
 }
 
-func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+func (s *server) Delete(ctx context.Context, req *deps.DeleteRequest) (*emptypb.Empty, error) {
 	log.Printf("Deleted user, id: %d", req.GetId())
 
 	return &emptypb.Empty{}, nil
@@ -60,7 +60,7 @@ func main() {
 
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterAuthV1Server(s, &server{})
+	deps.RegisterAuthV1Server(s, &server{})
 
 	log.Printf("server listening at #{lis.Addr()}")
 	if err = s.Serve(lis); err != nil {
