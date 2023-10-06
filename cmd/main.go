9fcc294064d56025daddedba74767e962c934c2e
Mikhail <rybalkov@transtelesoft.com>
2023-10-06 14:09:19 +0300

    Имплементированы методы

---
+++ cmd/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	desc "github.com/MikhailRibalkov/auth/grpc/pkg/auth_v1/pkg/auth_v1"
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
+)
+
+const grpcPort = ":8081"
+
+type server struct {
+	desc.UnimplementedAuthV1Server
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Client id: #{req.GetId}")
+
+	return &desc.GetResponse{
+		Info: &desc.UserInfo{
+			Id:        req.GetId(),
+			Name:      gofakeit.Name(),
+			Email:     gofakeit.Email(),
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			Role:      desc.UserRole_USER_ROLE_USER,
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Client id: #{req.GetId}")
+	return &desc.CreateResponse{}
+}
+
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("Client id: #{req.GetId}")
+	return &emptypb.Empty{}, nil
+
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Client id: #{req.GetId}")
+	return &emptypb.Empty{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		log.Fatal("Failed to listen: ", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterAuthV1Server(s, &server{})
+
+	log.Printf("server listening at #{lis.Addr()}")
+	if err = s.Serve(lis); err != nil {
+		log.Fatal("failed to serve: #{err}")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/MikhailRibalkov/auth/pkg/auth_v1
+
+go 1.21.1
+
+require (
+	google.golang.org/grpc v1.57.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
+)
